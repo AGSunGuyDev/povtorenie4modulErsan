52fafff4b3ebb141a6d2be28bc118ae46297539c
AGSunGuyDev <167071597+AGSunGuyDev@users.noreply.github.com>
2024-05-22 15:36:30 +0900

    Add files via upload

---
+++ mathfortest/mathfunc.cpp
@@ -0,0 +1,10 @@
+// §æ§å§ß§è§Ú§ñ
+#include <vector>
+
+std::vector<int> guh(int num, std::vector<int>& vec)
+{
+	for (int i = 0; i < vec.size(); i++) {
+		vec[i] += num;
+	}
+	return vec;
+}
---
+++ mathfortest/mathfunc.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+int factorial(int n);
+float divAtoB(float a, float b);
+bool isPositiv(int n);
+std::string reverseStr(std::string str);
